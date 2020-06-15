685c9a64363e9a7adb9c969a228aedf0956a6db7
Bob Caddy <r.Caddy@pitt.edu>
2020-06-15 16:29:31 -0400

    Burgers solver now runs to 0.3s

--- solver/burgers-solver.cpp
+++ solver/burgers-solver.cpp
@@ -42,7 +42,7 @@ int main()
     const int numGhosts        = 2;                                       // Number of ghost cells
     const int size             = PhysSize + 2 * numGhosts;                // total size of the array
     const double CFLNum        = 0.8;                                     // CFL Number
-    const double maxTime       = .25;                                     // Time to simlate to
+    const double maxTime       = .3;                                      // Time to simlate to
     const std::string LimType  = "MC";                                    // Limiter Type
     const std::string InitType = "vel-sine-pos";                          // Type of inital conditions
     //const std::string InitType = "vel-step";  // Type of inital conditions
