f6e44bc8c821ffd4fa12a37a3224a106ab32f7fc
Bob Caddy <r.Caddy@pitt.edu>
2020-06-26 07:13:42 -0400

    Implement Simulation1D::_setInitialConditions
    Currently only supports Sod shock tube initial conditions. Will add more
    in the future.
    
    Partially addresses issue #4

--- euler-solver/Simulation1D.cpp
+++ euler-solver/Simulation1D.cpp
@@ -2,6 +2,37 @@
 
 #include "Simulation1D.h"
 
+// =============================================================================
+void Simulation1D::_setInitialConditions(std::string const &initialConditionsKind)
+{
+    // Set up Sod initial conditions
+    // TODO Add other kinds of initial conditions
+
+    size_t const half  = grid.numTotCells / 2;
+
+    // Iterate over just the real cells on the left side
+    for (size_t i = grid.numGhostCells; 
+         i < half; 
+         i++)
+    {
+        grid.velocity[i] = 0.;
+        grid.density[i]  = 1.;
+        grid.pressure[i] = 1.;
+    }
+
+    // Iterate over the real cells on the right side
+    for (size_t i = half;
+         i < (grid.numTotCells - grid.numGhostCells);
+         i++)
+    {
+        grid.velocity[i] = 0;
+        grid.density[i]  = 0.125;  // 1/8th
+        grid.pressure[i] = 0.1;
+    }
+}
+// =============================================================================
+
+// =============================================================================
 void Simulation1D::computeTimeStep()
 {
     timeStep = std::abs(grid.velocity[grid.numGhostCells]);
@@ -17,3 +48,4 @@ void Simulation1D::computeTimeStep()
         }
     }
 }
+// =============================================================================
