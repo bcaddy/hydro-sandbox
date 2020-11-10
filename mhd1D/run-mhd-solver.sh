7ce37f44f1c5ee0c7b7a67ec9222ddf1e640ba60
Bob Caddy <r.Caddy@pitt.edu>
2020-11-10 16:13:28 -0500

    Upgrade run script to run the riemann tester too

--- mhd1D/run-mhd-solver.sh
+++ mhd1D/run-mhd-solver.sh
@@ -7,9 +7,30 @@
 #   it2dl
 #
 # Inputs:
-#   1: (optional) The framerate of the animation in fps
+#   1: -f (optional) The framerate of the animation in fps
+#   2: -t (optional) Test the riemann solver using the riemannTester.cpp program
 
 # set -x #echo all commands
+
+# Get options
+while getopts ":f:t:" opt; do
+    case $opt in
+        f)  # Set the FPS
+            fps = ${OPTARG}
+            ;;
+        t)  # Set the FPS
+            riemannTester = ${OPTARG}
+            ;;
+        \?)
+            echo "Invalid option: -${OPTARG}" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -${OPTARG} requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
 
 cd "${0%/*}"  # cd to hydro-sandbox directory
 
@@ -18,7 +39,7 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 cd "${REPO_ROOT}/mhd1D"
 
 echo -e "\nCompiling..."
-make
+make ${riemannTester}
 
 echo -e "\nRunning..."
 "./mhd-solver.exe"
@@ -26,7 +47,7 @@ echo -e "\nRunning..."
 cd "${REPO_ROOT}/visualization"
 
 echo -e "\n\nGenerating Plot..."
-./mhd-plotter.py ${1}
+./mhd-plotter.py ${fps}
 
 echo -e "\nDownloading Plot..."
 "${HOME}/.iterm2/it2dl" output-euler.mp4
