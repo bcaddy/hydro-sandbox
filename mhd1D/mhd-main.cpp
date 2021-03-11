8708276de6f45b594ea80be696e7547bd6318636
Bob Caddy <r.Caddy@pitt.edu>
2021-03-11 14:59:13 -0500

    typo

--- mhd1D/mhd-main.cpp
+++ mhd1D/mhd-main.cpp
@@ -54,8 +54,8 @@ int main()
     double const cfl                   = 0.4;
     double const maxTime               = 0.1; //0.1 for B&W shock tube, 0.2 for D&W and sod
     size_t const numRealCells          = 1'000;
-    std::string  initialConditionsKind = "dwShockTube";
-    std::string  boundaryConditions    = "dwShockTube";
+    std::string  initialConditionsKind = "bwShockTube";
+    std::string  boundaryConditions    = "bwShockTube";
     std::string  reconstructionKind    = "PLM";
     std::string  limiterKind           = "MC";  // Options: zeroSlope, centerDiff, minMod, or MC
     std::string  riemannSolverKind     = "HLLD";  // Options: "HLLD"
