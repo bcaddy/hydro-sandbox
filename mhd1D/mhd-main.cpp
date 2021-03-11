4790aefe013619e59bc7497f834c5e36a1061f28
Bob Caddy <r.Caddy@pitt.edu>
2021-03-11 13:34:45 -0500

    update main for bw shock tube tests

--- mhd1D/mhd-main.cpp
+++ mhd1D/mhd-main.cpp
@@ -50,12 +50,12 @@ int main()
 
     // ===== Settings ==========================================================
     double const physicalLength        = 1.;
-    double const gamma                 = 1.4;  // 5./3. for most things, chollaSod uses 1.4
+    double const gamma                 = 5./3.;  // 5./3. for most things, chollaSod uses 1.4
     double const cfl                   = 0.4;
-    double const maxTime               = 0.2; //0.1 for B&W shock tube, 0.2 for D&W and sod
+    double const maxTime               = 0.1; //0.1 for B&W shock tube, 0.2 for D&W and sod
     size_t const numRealCells          = 1'000;
-    std::string  initialConditionsKind = "chollaSodShockTube";
-    std::string  boundaryConditions    = "chollaSodShockTube";
+    std::string  initialConditionsKind = "bwShockTube";
+    std::string  boundaryConditions    = "bwShockTube";
     std::string  reconstructionKind    = "PLM";
     std::string  limiterKind           = "MC";  // Options: zeroSlope, centerDiff, minMod, or MC
     std::string  riemannSolverKind     = "HLLD";  // Options: "HLLD"
