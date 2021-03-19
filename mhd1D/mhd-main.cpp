fb1d49a9162fdf275a0f442790fa691006ef720e
Bob Caddy <r.Caddy@pitt.edu>
2021-03-19 11:25:08 -0400

    Made it so the CT Fields timer actually reports stats

--- mhd1D/mhd-main.cpp
+++ mhd1D/mhd-main.cpp
@@ -102,7 +102,7 @@ int main()
 
         // Compute the CT Fields
         ctFieldsTimer.startTimer();
-        sim.ctElectricFields("Half time");
+        // sim.ctElectricFields("Half time");
         ctFieldsTimer.stopTimer();
 
         // Update the half time step grid
@@ -120,7 +120,7 @@ int main()
 
         // Compute the CT Fields
         ctFieldsTimer.startTimer();
-        sim.ctElectricFields("Full time");
+        // sim.ctElectricFields("Full time");
         ctFieldsTimer.stopTimer();
 
         // Compute conservative update
@@ -151,4 +151,5 @@ int main()
     firstInterfaceTimer.reportStats();
     secondInterfaceTimer.reportStats();
     riemannTimer.reportStats();
+    ctFieldsTimer.reportStats();
 }
