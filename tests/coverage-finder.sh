d2ef94e9997707309141063d25fcedfb06148347
Bob Caddy <r.Caddy@pitt.edu>
2021-08-17 11:32:15 -0400

    clean up formatting

--- tests/coverage-finder.sh
+++ tests/coverage-finder.sh
@@ -12,12 +12,12 @@ make
 # --directory = where the program is? can be done more than once
 # --output-file = results output file. Should end in ".info"
 lcov --capture --directory . --output-file coverage.info
-# --remove TRACEFILE PATTERN = remove all things associated with PATTERN in TRACEFILE
+
 exclude_patterns=('/usr/*'      # Remove everything from /usr/
                   '/Library/*'  # Remove everything from /Library/
                   '*-tests.cpp' # Remove traces of the tests themselves
                   '*-test.cpp') # Remove traces of the tests themselves
-
+# --remove TRACEFILE PATTERN = remove all things associated with PATTERN in TRACEFILE
 lcov --remove coverage.info "${exclude_patterns[@]}" --output-file coverage.info  # Remove traces of the tests themselves
 
 lcov --list coverage.info
