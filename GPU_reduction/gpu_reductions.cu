6bc335fe55d24d4cfb6f39822e0bd8284ddcc6a1
Bob Caddy <r.Caddy@pitt.edu>
2022-04-12 13:36:59 -0400

    Working version of sum reduction

--- GPU_reduction/gpu_reductions.cu
+++ GPU_reduction/gpu_reductions.cu
@@ -14,37 +14,30 @@
 // STL Includes
 #include <stdio.h>
 #include <cstdint>
+#include <iostream>
+#include <vector>
 
 // Local Includes
+
+#define WARPSIZE 32
+static constexpr int maxWarpsPerBlock = 1024/WARPSIZE; // outside kernel
 
 // Done editing for CUDA/HIP
 __inline__ __device__ int warpReduceSum(int val)
 {
-    #ifdef O_HIP
-        uint64_t const mask = 0xFFFFFFFFFFFFFFFF;
-    #else
-        uint32_t const mask = 0xFFFFFFFF;
-    #endif  //HIP
-
     for (int offset = warpSize/2; offset > 0; offset /= 2)
     {
-        val += __shfl_down_sync(mask, val, offset);
+        val += __shfl_down_sync(0xFFFFFFFF,val, offset);
     }
-
     return val;
 }
 
-
 __inline__ __device__ int blockReduceSum(int val)
 {
+    __shared__ int shared[::maxWarpsPerBlock]; // Shared memory for storing the results of each warp-wise partial reduction
 
-    int const numWarps = blockDim.x/warpSize;
-
-    __shared__ int shared[numWarps]; // Shared memory for storing the results of each warp-wise partial reduction
-
-    // TODO STOPPED HERE LAST NIGHT
-    int lane = threadIdx.x % warpSize;
-    int warpId = threadIdx.x / warpSize;
+    int lane   = threadIdx.x % warpSize;  // thread ID within the warp,
+    int warpId = threadIdx.x / warpSize;  // ID of the warp itself
 
     val = warpReduceSum(val);     // Each warp performs partial reduction
 
@@ -53,14 +46,21 @@ __inline__ __device__ int blockReduceSum(int val)
     __syncthreads();              // Wait for all partial reductions
 
     //read from shared memory only if that warp existed
-    val = (threadIdx.x < blockDim.x / warpSize) ? shared[lane] : 0;
+    val = (threadIdx.x < ::maxWarpsPerBlock) ? shared[lane] : 0;
 
     if (warpId==0) val = warpReduceSum(val); //Final reduce within first warp
 
     return val;
 }
 
-
+/*!
+ * \brief Find the sum of the array
+ *
+ * \param[in] in The input array
+ * \param[out] out The output scalar variable
+ * \param[in] N the number of elements in the `in` array
+ * \return __global__
+ */
 __global__ void deviceReduceBlockAtomicKernel(int *in, int* out, int N)
 {
     // Initialize sum
@@ -99,8 +99,25 @@ int	main()
 {
     int const numBlocks       = 2;
     int const threadsPerBlock = 1024;
-    checkDims<<<numBlocks, threadsPerBlock>>>();
+    // checkDims<<<numBlocks, threadsPerBlock>>>();
+
+    size_t const size = 256*256*256;
+    int host_sum;
+    int *dev_vec, *dev_sum;
+    std::vector<int> host_vec(size, 1);
+
+    cudaMalloc(&dev_vec, host_vec.size() * sizeof(int));
+    cudaMalloc(&dev_sum, sizeof(int));
+    cudaMemcpy(dev_vec, host_vec.data(), host_vec.size() * sizeof(int), cudaMemcpyHostToDevice);
+
+    deviceReduceBlockAtomicKernel<<<numBlocks, threadsPerBlock>>>(dev_vec, dev_sum, host_vec.size());
+
+    cudaMemcpy(&host_sum, dev_sum, sizeof(int), cudaMemcpyDeviceToHost);
+
     cudaDeviceSynchronize();
+
+
+    std::cout << "The final result should be " << host_vec.size() << " and is " << host_sum << std::endl;
 
     return 0;
 }
